f26f6d0b780f6251c8564acc02292b35391220cf
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2024-05-24 19:48:14 +0000

    grammar: support import decl

--- bsc/__main__.py
+++ bsc/__main__.py
@@ -2,14 +2,13 @@
 # source code is governed by an MIT license that can be found in the 
 # LICENSE file.
 
-from lark import Lark, Transformer, v_args
+from lark import Lark
+
+from astgen import AstGen
 
 with open("bsc/bluescript.lark", "r") as bs_grammar_file:
     bs_grammar = bs_grammar_file.read()
 
 bs_parser = Lark(bs_grammar, parser='earley')
-# Disabling propagate_positions and placeholders slightly improves speed
-#propagate_positions=False,
-#maybe_placeholders=False,
-# Using an internal transformer is faster and more memory efficient
-print(bs_parser.parse(open("examples/hello_world.bs").read()))
+ast = AstGen(True).transform(bs_parser.parse(open("examples/syntax.bs").read()))
+print(ast)
---
+++ bsc/astgen.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2024 Jose Mendoza. All rights reserved. Use of this 
+# source code is governed by an MIT license that can be found in the 
+# LICENSE file.
+
+from lark import Transformer
+
+class AstGen(Transformer):
+    pass
